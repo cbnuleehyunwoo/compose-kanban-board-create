5a7ee39ba076ae29e5c905305329c2796ee977d4
cbnuLeehyunwoo <henow123@naver.com>
2026-03-12 19:50:08 +0900

    chore: 불필요한 주석 삭제

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/taskmodal/ModalBody.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/taskmodal/ModalBody.kt
@@ -46,7 +46,6 @@ fun ModalBody(state: ModalCreateFormState, modifier: Modifier = Modifier) {
             },
             isValid = true,
         )
-//
         ModalBodyInput(
             title = "태그",
             essential = false,
