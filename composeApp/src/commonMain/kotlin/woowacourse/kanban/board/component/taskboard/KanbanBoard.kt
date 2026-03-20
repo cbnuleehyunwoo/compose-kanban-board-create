7328525dbb54b4e4441676bfaf3c2b7b35122d14
cbnuLeehyunwoo <henow123@naver.com>
2026-03-20 17:55:47 +0900

    feat: 칸반 태스크 보드 UI 구현
    - 태스크 보드의 컴포넌트를 조합하는 KanbanBoard를 구현하였습니다.
    - 추가적으로 다이얼로그 창을 띄우는 로직을 구현하였습니다.

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/taskboard/KanbanBoard.kt
@@ -0,0 +1,66 @@
+package woowacourse.kanban.board.component.taskboard
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.component.taskmodal.ModalCreateFormState
+
+@Composable
+fun KanbanBoard(modifier: Modifier = Modifier) {
+    var showDialog by remember { mutableStateOf(false) }
+    val state = remember { ModalCreateFormState() }
+    Column(modifier = modifier) {
+        Box {
+            if (showDialog) {
+                KanbanBoardDialog(
+                    onClickCancel = { showDialog = false },
+                    onClickConfirm = {},
+                )
+            }
+        }
+        KanbanTaskBoardHeader(
+            onClick = { showDialog = !showDialog },
+            completion = 0.5f,
+            modifier = modifier
+                .border(
+                    width = 1.dp,
+                    color = Color.LightGray,
+                )
+                .padding(24.dp),
+        )
+        Row(
+            modifier = modifier
+                .background(color = Color(0xFFF9FAFB))
+                .padding(24.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            KanbanCardHolder()
+            KanbanCardHolder()
+            KanbanCardHolder()
+        }
+    }
+
+}
+
+@Preview(
+    showBackground = true,
+    widthDp = 1300,
+    heightDp = 910,
+)
+@Composable
+fun KanbanBoardPreview() {
+    KanbanBoard()
+}
